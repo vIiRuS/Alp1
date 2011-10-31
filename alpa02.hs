aa565e316aad1150238ef8eba9e7513b2557a3ed
Phillip Thelen <viirus@pherth.net>
2011-10-31 12:22:58 +0100

    Nerdware license

--- alpa02.hs
+++ alpa02.hs
@@ -1,9 +1,10 @@
--- ----------------------------------------------------------------------------
--- "THE BEER-WARE LICENSE" (Revision 42):
--- <viirus@pherth.net> wrote this file. As long as you retain this notice you
+-- -----------------------------------------------------------------------------
+-- "THE NERD-WARE LICENSE" (Revision 1):
+-- [<viirus@pherth.net>] wrote this file. As long as you retain this notice you
 -- can do whatever you want with this stuff. If we meet some day, and you think
--- this stuff is worth it, you can buy me a beer in return Phillip Thelen
--- ----------------------------------------------------------------------------
+-- this stuff is worth it, you can buy me/us a beer, Mate or some Food in return
+-- [Phillip Thelen]
+-- -----------------------------------------------------------------------------
 
 -- Aufgabe 1
 zeitdifferenz :: (Int, Int) -> (Int, Int) -> (Int, Int)
