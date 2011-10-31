d97d3550812b1f1c1515e08167a50a78d8700235
Phillip Thelen <viirus@pherth.net>
2011-10-31 11:33:28 +0100

    Lizenzänderung

--- alpa01.hs
+++ alpa01.hs
@@ -1,3 +1,10 @@
+-- ----------------------------------------------------------------------------
+-- "THE BEER-WARE LICENSE" (Revision 42):
+-- <viirus@pherth.net> wrote this file. As long as you retain this notice you
+-- can do whatever you want with this stuff. If we meet some day, and you think
+-- this stuff is worth it, you can buy me a beer in return Phillip Thelen
+-- ----------------------------------------------------------------------------
+
 -- ALP I - Übungszettel 1
 
 -- Aufgabe I: Dreiecke (10 Punkte) 
