f81f4e7c9c9dc13da6e05f794d3c4baaf3f1ad42
laerador <laerador@gmail.com>
2012-01-03 12:21:32 +0100

    Test aus der eigentlichen Funktion entfernt
    TestBaum aus der eigentlichen Funktion 'entfernt' und unten
    angehangen. (Mit Kommentar)

--- alpa08.hs
+++ alpa08.hs
@@ -3,12 +3,13 @@ import Data.List
 
 data Tree a = Leaf a | Branch a (Tree a) (Tree a)
 
-testTree :: (Tree Int)
-testTree = Branch 29 (Branch 14 (Leaf 2) (Leaf 19)) (Branch 32 (Leaf 30) (Leaf 100))
-
 listTree :: Ord a => (Tree a) -> [a]
 listTree (Branch a b1 b2) = (listTree b1) ++ [a] ++ (listTree b2)
 listTree (Leaf a) = [a] 
 
 isSearchTree :: (Tree Int) -> Bool
 isSearchTree b = listTree b == sort (listTree b)
+
+-- zum Testen:
+testTree :: (Tree Int)
+testTree = Branch 29 (Branch 14 (Leaf 2) (Leaf 19)) (Branch 32 (Leaf 30) (Leaf 100))
