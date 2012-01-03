7cf4a14fdbd5df15fd1d96122b6a6517f40ad281
laerador <laerador@gmail.com>
2012-01-03 11:57:36 +0100

    Leerzeichen hinzugefügt

--- alpa08.hs
+++ alpa08.hs
@@ -6,7 +6,7 @@ data Tree a = Leaf a | Branch a (Tree a) (Tree a)
 testTree :: (Tree Int)
 testTree = Branch 29 (Branch 14 (Leaf 2) (Leaf 19)) (Branch 32 (Leaf 30) (Leaf 100))
 
-listTree :: Ord a => (Tree a)->[a]
+listTree :: Ord a => (Tree a) -> [a]
 listTree (Branch a b1 b2) = (listTree b1) ++ [a] ++ (listTree b2)
 listTree (Leaf a) = [a] 
 
